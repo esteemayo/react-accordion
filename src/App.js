393302424f4e3ac548fe203f951afe31dbcdf897
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-05-08 17:50:19 +0100

    Completed the react accordion app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Question from "./components/Question";
+import data from "./services/questionService";
+
+import "./App.css";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <Question key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
+    </main>
   );
 }
 
