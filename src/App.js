70d73f94baaa6bf4a13d133b55915ebb681ccb6a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-27 01:06:41 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-import Question from "components/Question";
-import data from "services/questionService";
+import Question from 'components/Question';
+import data from 'services/questionService';
 
 import "./App.css";
 
